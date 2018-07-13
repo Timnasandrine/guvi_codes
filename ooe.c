40a1e074e878fd4362b8e9b1661ffead3b3d45cc
Timnasandrine <40801383+Timnasandrine@users.noreply.github.com>
2018-07-13 12:38:51 +0530

    Create ooe.c

---
+++ ooe.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number to check odd or even:");
+scanf("%d",&n);
+
+if(n % 2==0)
+{
+printf("\n even");
+}
+else
+{
+printf("\n odd");
+}
+return 0;
+}
